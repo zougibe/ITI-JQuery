be7d4a08a90e0d3b025e18030a8cb825e8d44042
Ahmed (Zougibe) Mohamed <84062148+zougibe@users.noreply.github.com>
2025-01-03 21:41:42 +0200

    Add files via upload

--- Day 2/script3.js
+++ Day 2/script3.js
@@ -1,37 +1,68 @@
 
-const images = [
-    "../../Client Side Tech/images/slide1.jpg",
-    "../../Client Side Tech/images/slide2.jpg",
-    "../../Client Side Tech/images/slide3.jpg",
-    "../../Client Side Tech/images/slide4.jpg"
-];
+    const audio = $("audio")[0];
+    const playBtn = $(".Play");
+    const stopBtn = $(".Stop");
+    const volumeSlider = $(".volume");
+    const muteBtn = $("#mute-unmute");
+    const seekSlider = $(".Seek");
+    let currentSong = "";
 
-let currentIndex = 0;
+    $(".play-song").on("click", function () {
+        const songSrc = $(this).parent().data("src");
+        if (currentSong !== songSrc) {
+            audio.src = songSrc;
+            currentSong = songSrc;
+        }
+        audio.play();
+        playBtn.html('<i class="fa-solid fa-pause"></i>');
+    });
 
-
-$(".slider").click(function (e) {
-    const src = $(this).attr("src");
-    $("#mainP").attr("src", src); 
-    $(".mainPic").show(); 
-    currentIndex = images.indexOf(src);
-});
-
-
-$(".mainPic").click(function (e) {
-    if (e.target === this) {
-        $(".mainPic").hide(); 
-    }
-});
-
-
-$(".last").click(function () {
-    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
-    $("#mainP").attr("src", images[currentIndex]);
-});
+    playBtn.on("click", function () {
+        if (audio.paused || audio.ended) {
+            audio.play();
+            playBtn.html('<i class="fa-solid fa-pause"></i>');
+        } else {
+            audio.pause();
+            playBtn.html('<i class="fa-solid fa-play"></i>');
+        }
+    });
 
 
-$(".next").click(function () {
-    currentIndex = (currentIndex + 1) % images.length;
-    $("#mainP").attr("src", images[currentIndex]);
-});
+    stopBtn.on("click", function () {
+        audio.pause();
+        audio.onload()
+        playBtn.html('<i class="fa-solid fa-play"></i>');
+    });
+
+    volumeSlider.on("input", function () {
+        audio.volume = $(this).val();
+    });
+
+
+    muteBtn.on("click", function () {
+        audio.muted = !audio.muted;
+        $(this).html(
+            audio.muted ? '<i class="fa-solid fa-volume-xmark"></i>': '<i class="fa-solid fa-volume-high"></i>'
+        );
+    });
+
+
+    seekSlider.on("input", function () {
+        const seekTo = ($(this).val() / 100) * audio.duration;
+        audio.currentTime = seekTo;
+        
+    });
+
+
+    audio.addEventListener("timeupdate", function () {
+        if (audio.duration) {
+            const progress = (audio.currentTime / audio.duration) * 100;
+            seekSlider.val(progress);
+        }
+    });
+
+    audio.addEventListener("ended", function () {
+        seekSlider.val(0);
+        playBtn.html('<i class="fa-solid fa-play"></i>');
+    });
 
